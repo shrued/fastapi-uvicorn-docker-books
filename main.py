e1c3c7727fd21bb382f7207c0672230d73943c27
shrued <shrutisara10@gmail.com>
2020-10-29 15:43:17 +0530

    fastapi-uvicorn

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-  return (message: "Hi!")
+db = []
+
+class Book(BaseModel):
+  title: str
+  author: str 
+
+@app.get('/')
+def index():
+  return {'key' : 'value'}
+
+@app.get('/books')
+def get_books():
+  return db
+
+@app.get('/books/{book_id}')
+def get_book(book_id: int):
+  return db[book_id-1]
+
+@app.post('/books')
+def create_book(book: Book):
+  db.append(book.dict())
+  return db[-1]
+
+@app.delete('/books/{book_id}')
+def delete_book(book_id: int):
+  db.pop(book_id-1)
+  return {}
